7d3dd7fac49d9b7525f2fac8900e7358de786f79
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2022-11-24 18:54:24 +0600

    create bmw api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app =express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -11,18 +11,58 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.xa1zyf9.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const collection = client.db("carModels").collection("cars");
+const userCollection = client.db("userCollection").collection("user");
+const carQuality = client.db("carQuelity").collection("carquelity");
+const bmwCollection = client.db("bmwCollection").collection("bmw");
+const audiCollection = client.db("audiCollaction").collection("audi");
 
-app.post('/carsModels', async(req, res)=>{
+app.post('/users', async(req, res)=>{
+    try{
     const data = req.body;
-    const result = await collection.insertMany(data);
+    // console.log(data);
+    const result = await userCollection.insertOne(data);
     res.send(result);
+    }
+    catch(e){
+        console.log(e.message);
+    }
 })
 
+app.get('/carQuality', async(req, res)=>{
+    try{
+        const query = {};
+        const result = await carQuality.find(query).toArray();
+        res.send(result);
+    }
+    catch(e){
+        console.log(e.message);
+    }
+})
+// bmw section
+app.get('/bmw', async(req, res)=>{
+    try{
+        const query = {};
+        const result = await bmwCollection.find(query).toArray();
+        res.send(result);
+    }
+    catch(e){
+        console.log(e.message);
+    }
+})
 
-
+app.get('/bmwDetail/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await bmwCollection.findOne(query);
+    res.send(result);
+})
+// audi section
+app.get('/audi', async(req, res)=>{
+    const query = {};
+    const result = await audiCollection.find(query).toArray();
+    res.send(result);
+})
 
 app.get('/', (req, res)=>{
     res.send('server is running') 
